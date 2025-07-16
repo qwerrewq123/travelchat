7cdc36977694fdcee39153c7b9533caeedc25c3b
qwerrewq123 <ccccssung@gmail.com>
2025-07-16 10:05:31 +0900

    first commit

---
+++ src/main/java/com/ardenter/travelchat/TravelchatApplication.java
@@ -0,0 +1,13 @@
+package com.ardenter.travelchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ardenter/travelchat/TravelchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardenter.travelchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
